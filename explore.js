d7d49b202bb29c3c42dae02878fae0dc4de9805c
Sarthak Joshi <sarthak987joshi@outlook.com>
2020-10-22 22:31:29 +0530

    Celeb Explore Service 1.0.0 (Database Connection Updated)

--- explore.js
+++ explore.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const ExploreCelebsRoutesAPI = require('./routes/ExploreCelebsServiceRoutes');
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8001;
 
 const app = express();
 
